e5e6d5a55257eed4752cf0f2173f82862b952196
Victor Afolabi <masteryomi@gmail.com>
2016-11-17 07:58:39 +0100

    changed the users and added config files

--- credentials.js
+++ credentials.js
@@ -19,29 +19,29 @@ module.exports = {
 
   // MongoDB
   database: {
-    url : process.env.MONGODB || "mongodb://USERNAME:PASSWORD@HOST:PORT/DB_NAME"
+    url : process.env.MONGODB ||"mongodb://c847809c-9ab4-43e7-b50a-50af4cb6c88a:3955b5f3-e2ec-41f3-8672-80b6b1224a9c@192.155.247.230:10168/db"
   },
 
   tradeoff_analytics : {
-    username : process.env.TRADEOFF_ANALYTICS_USERNAME || '<username>',
-    password : process.env.TRADEOFF_ANALYTICS_PASSWORD || '<password>',
+    username : process.env.TRADEOFF_ANALYTICS_USERNAME || '8d2bf472-1931-4e97-8c02-2c63a5c03758',
+    password : process.env.TRADEOFF_ANALYTICS_PASSWORD || 'dGQnCI3bE62r',
     version  : 'v1'
   },
 
   personality_insights : {
-    username : process.env.PERSONALITY_INSIGHTS_USERNAME || '<username>',
-    password : process.env.PERSONALITY_INSIGHTS_PASSWORD || '<password>',
+    username : process.env.PERSONALITY_INSIGHTS_USERNAME || 'b252ad6b-f56a-4ca8-ae28-e39910451a70',
+    password : process.env.PERSONALITY_INSIGHTS_PASSWORD || '0HaqjdnKl8z6',
     version  : 'v2'
   },
 
   // Twitter app credentials: https://apps.twitter.com/app
   twitter: process.env.TWITTER ? JSON.parse(process.env.TWITTER) : [
     {
-      consumer_key :        '<consumer_key>',
-      consumer_secret  :    '<consumer_secret>',
-      access_token_key :    '<access_token_key>',
-      access_token_secret : '<access_token_secret>',
-      enabled : false // Disabled since this is just an example
+      consumer_key :        'mVz1Tfeuecui5Wr7s6OcWOtTM',
+      consumer_secret  :    'uE3BwrkyxkjETsa0TthJb4IYjSkUPzEdboyxdTDDzzdmjavFmS',
+      access_token_key :    '143414204-Gs9Zon3JQZMaYqkN5kK9ZIYQDqtz20MajeaRNzj5',
+      access_token_secret : 'ayL6TgPa8vGDhqXvWxv12wSuA3BItPcu2TQLWUC71R9qH',
+      enabled : true // Disabled since this is just an example
     }
   ]
 };
